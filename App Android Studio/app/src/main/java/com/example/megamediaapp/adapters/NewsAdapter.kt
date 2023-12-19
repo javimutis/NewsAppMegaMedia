e343b3fd1b4361d20fcf4546f66d81e4dc0308fb
Javiera Mutis <122648544+javimutis@users.noreply.github.com>
2023-12-19 13:10:03 -0300

    Pruebas unitarias y flecha atrás

--- App Android Studio/app/src/main/java/com/example/megamediaapp/adapters/NewsAdapter.kt
+++ App Android Studio/app/src/main/java/com/example/megamediaapp/adapters/NewsAdapter.kt
@@ -18,67 +18,61 @@ class NewsAdapter(
     private val onNewsClickListener: OnNewsClickListener
 ) : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
-    // Método para crear una nueva vista para mostrar los elementos de la lista.
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
         return NewsViewHolder(itemView)
     }
 
-    // Método para asociar datos a cada elemento de la vista (ViewHolder).
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val currentNews = newsList[position]
         holder.bind(currentNews)
     }
 
-    // Método para obtener la cantidad de elementos en la lista.
     override fun getItemCount(): Int {
         return newsList.size
     }
 
     inner class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-        private val descriptionTextView: TextView = itemView.findViewById(R.id.descriptionTextView)
-        private val imageView: ImageView = itemView.findViewById(R.id.imageView)
+        private val newsTitleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        private val newsDescriptionTextView: TextView = itemView.findViewById(R.id.descriptionTextView)
+        private val newsImageView: ImageView = itemView.findViewById(R.id.imageView)
         private val playImageView: ImageView = itemView.findViewById(R.id.playImageView)
 
         fun bind(news: News) {
-            titleTextView.text = news.title
-            descriptionTextView.text = news.description
+            newsTitleTextView.text = news.title
+            newsDescriptionTextView.text = news.description
 
             if (news.imageUrl.isNotEmpty()) {
                 loadImageFromUrl(news.imageUrl)
             } else {
-                imageView.setImageResource(R.drawable.baseline_crop_square_24)
+                newsImageView.setImageResource(R.drawable.newsapp) // Usa un recurso específico para la imagen por defecto
             }
 
-            // Clic en la imagen para reproducir el video
-            imageView.setOnClickListener {
+            // Gestiona el clic en la imagen para reproducir el video si la URL está disponible
+            newsImageView.setOnClickListener {
                 if (news.videoUrl.isNotEmpty()) {
                     onNewsClickListener.onNewsClick(news)
                 }
             }
 
-            if (news.videoUrl.isNotEmpty()) {
-                playImageView.visibility = View.VISIBLE
-            } else {
-                playImageView.visibility = View.GONE
-            }
+            // Muestra u oculta el icono de reproducción según la disponibilidad de la URL de video
+            playImageView.visibility = if (news.videoUrl.isNotEmpty()) View.VISIBLE else View.GONE
         }
 
         private fun loadImageFromUrl(imageUrl: String) {
             val requestOptions = RequestOptions()
-                .placeholder(R.drawable.baseline_crop_square_24) // Placeholder mientras carga la imagen
+                .placeholder(R.drawable.baseline_crop_square_24) // Placeholder mientras se carga la imagen
 
             val glideUrl = GlideUrl(imageUrl, LazyHeaders.Builder().addHeader("User-Agent", "your-user-agent").build())
 
             Glide.with(itemView.context)
                 .load(glideUrl)
                 .apply(requestOptions)
-                .into(imageView)
+                .into(newsImageView)
         }
     }
 
-    // Interfaz para gestionar el clic en un elemento de la lista.
+    // Interfaz para gestionar el clic en un elemento de la lista
     interface OnNewsClickListener {
         fun onNewsClick(news: News)
     }
